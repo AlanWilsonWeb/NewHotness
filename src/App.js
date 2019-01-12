727f7672d446d04391961f371f763896fc323d8f
Unknown <nannerfox@me.com>
2019-01-12 11:32:29 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="boxx"></div>
+        <div id="top"><h1>This is the Header</h1></div>
+        <div id="content"><h2>this is the body</h2></div>
+        <div id="bottom"><h5 id="footerMark">Nannerfox Productions</h5></div>
       </div>
     );
   }
